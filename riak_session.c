ba1964ff98d33f782444da585a929aa6d2df0a41
Anthon Pang <anthon.pang@gmail.com>
2017-01-05 06:17:10 +0000

    riak_session.c

--- riak_session.c
+++ riak_session.c
@@ -34,30 +34,30 @@
 #include <main/php_variables.h>
 
 #define SET_GET_OPTION_IF_PRESENT(NAME, CLIENT, ZARR, ZPP) \
-    if (zend_hash_find(Z_ARRVAL_P(ZARR), NAME, sizeof(NAME), (void**)&ZPP) == SUCCESS) { \
+    if ((ZPP = zend_hash_find(Z_ARRVAL(ZARR), NAME)) != NULL) { \
         convert_to_long(*ZPP); \
         zend_update_property(riak_get_input_ce, CLIENT, NAME, sizeof(NAME)-1, *ZPP TSRMLS_CC); \
     }
 
 #define SET_PUT_OPTION_IF_PRESENT(NAME, CLIENT, ZARR, ZPP) \
-    if (zend_hash_find(Z_ARRVAL_P(ZARR), NAME, sizeof(NAME), (void**)&ZPP) == SUCCESS) { \
+    if ((ZPP = zend_hash_find(Z_ARRVAL(ZARR), NAME)) != NULL) { \
         convert_to_long(*ZPP); \
         zend_update_property(riak_put_input_ce, CLIENT, NAME, sizeof(NAME)-1, *ZPP TSRMLS_CC); \
     }
 
 #define SET_DELETE_OPTION_IF_PRESENT(NAME, CLIENT, ZARR, ZPP) \
-    if (zend_hash_find(Z_ARRVAL_P(ZARR), NAME, sizeof(NAME), (void**)&ZPP) == SUCCESS) { \
+    if ((ZPP = zend_hash_find(Z_ARRVAL(ZARR), NAME)) != NULL) { \
         convert_to_long(*ZPP); \
         zend_update_property(riak_delete_input_ce, CLIENT, NAME, sizeof(NAME)-1, *ZPP TSRMLS_CC); \
     }
 
 typedef struct _riak_session_data {
-   char* session_name;
-   zval *zbucket;
-   zval *zclient;
-   zval *zgetprops;
-   zval *zputprops;
-   zval *zdelprops;
+    char* session_name;
+    zval zbucket;
+    zval zclient;
+    zval zgetprops;
+    zval zputprops;
+    zval zdelprops;
 } riak_session_data;
 
 PS_OPEN_FUNC(riak);
@@ -68,100 +68,101 @@ PS_DESTROY_FUNC(riak);
 PS_GC_FUNC(riak);
 
 ps_module ps_mod_riak = {
-	PS_MOD(riak)
+    PS_MOD(riak)
 };
 
 #define PS_RIAK_DATA riak_session_data *data = PS_GET_MOD_DATA()
 
 PS_OPEN_FUNC(riak) /* {{{ */
 {
-    riak_session_data* session_data;
-    zval *zclient, *zbucket, *zgetprops, *zdelprops, *zputprops, *zoptionsarray, **zoption;
+    riak_session_data* data;
+    zval zclient, zbucket, zgetprops, zdelprops, zputprops, zoptionsarray, zoption;
     php_url *purl;
     char* stripped_path;
 
     purl = php_url_parse(save_path);
 
-    if (!purl) {
-        return FAILURE;
+    if ( ! purl) {
+        goto cleanup;
     }
 
-    zclient = create_client_object(purl->host, purl->port TSRMLS_CC);
+    data = ecalloc(1, sizeof(riak_session_data));
+
+    if ( ! data) {
+        goto cleanup_purl;
+    }
+
+    create_client_object(&data->zclient, purl->host, purl->port TSRMLS_CC);
 
     if (EG(exception)) {
-        php_url_free(purl);
-        zval_ptr_dtor(&zclient);
-
-        PS_SET_MOD_DATA(NULL);
-
-        return FAILURE;
+        goto cleanup_client;
     }
 
     // Input options for get operations
-    MAKE_STD_ZVAL(zgetprops);
-    object_init_ex(zgetprops, riak_get_input_ce);
-
-    MAKE_STD_ZVAL(zputprops);
-    object_init_ex(zputprops, riak_put_input_ce);
-
-    MAKE_STD_ZVAL(zdelprops);
-    object_init_ex(zdelprops, riak_delete_input_ce);
+    object_init_ex(&data->zgetprops, riak_get_input_ce);
+    object_init_ex(&data->zputprops, riak_put_input_ce);
+    object_init_ex(&data->zdelprops, riak_delete_input_ce);
 
     /* Set w,dw,pw,r,rw,pw if set */
-    MAKE_STD_ZVAL(zoptionsarray);
-    array_init(zoptionsarray);
+    array_init(&zoptionsarray);
 
     if (purl->query) {
-        sapi_module.treat_data(PARSE_STRING, estrdup(purl->query), zoptionsarray TSRMLS_CC);
+        sapi_module.treat_data(PARSE_STRING, estrdup(purl->query), &zoptionsarray TSRMLS_CC);
 
-        SET_GET_OPTION_IF_PRESENT("r", zgetprops, zoptionsarray, zoption)
-        SET_GET_OPTION_IF_PRESENT("rw", zgetprops, zoptionsarray, zoption)
-        SET_GET_OPTION_IF_PRESENT("pr", zgetprops, zoptionsarray, zoption)
+        SET_GET_OPTION_IF_PRESENT("r", &data->zgetprops, zoptionsarray, zoption)
+        SET_GET_OPTION_IF_PRESENT("rw", &data->zgetprops, zoptionsarray, zoption)
+        SET_GET_OPTION_IF_PRESENT("pr", &data->zgetprops, zoptionsarray, zoption)
 
-        SET_PUT_OPTION_IF_PRESENT("w", zputprops, zoptionsarray, zoption)
-        SET_PUT_OPTION_IF_PRESENT("dw", zputprops, zoptionsarray, zoption)
-        SET_PUT_OPTION_IF_PRESENT("pw", zputprops, zoptionsarray, zoption)
+        SET_PUT_OPTION_IF_PRESENT("w", &data->zputprops, zoptionsarray, zoption)
+        SET_PUT_OPTION_IF_PRESENT("dw", &data->zputprops, zoptionsarray, zoption)
+        SET_PUT_OPTION_IF_PRESENT("pw", &data->zputprops, zoptionsarray, zoption)
 
-        SET_DELETE_OPTION_IF_PRESENT("r", zdelprops, zoptionsarray, zoption)
-        SET_DELETE_OPTION_IF_PRESENT("pr", zdelprops, zoptionsarray, zoption)
-        SET_DELETE_OPTION_IF_PRESENT("rw", zdelprops, zoptionsarray, zoption)
-        SET_DELETE_OPTION_IF_PRESENT("w", zdelprops, zoptionsarray, zoption)
-        SET_DELETE_OPTION_IF_PRESENT("dw", zdelprops, zoptionsarray, zoption)
-        SET_DELETE_OPTION_IF_PRESENT("pw", zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("r", &data->zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("pr", &data->zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("rw", &data->zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("w", &data->zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("dw", &data->zdelprops, zoptionsarray, zoption)
+        SET_DELETE_OPTION_IF_PRESENT("pw", &data->zdelprops, zoptionsarray, zoption)
     }
 
-    zval_ptr_dtor(&zoptionsarray);
+    zval_dtor(&zoptionsarray);
 
     stripped_path = php_trim(purl->path, strlen(purl->path), "/", 1, NULL, 3 TSRMLS_CC);
-    zbucket = create_bucket_object(zclient, stripped_path, strlen(stripped_path) TSRMLS_CC);
 
-    php_url_free(purl);
+    create_bucket_object(&data->bucket, &data->zclient, stripped_path, strlen(stripped_path) TSRMLS_CC);
+
     efree(stripped_path);
 
     if (EG(exception)) {
-        zval_ptr_dtor(&zbucket);
-        zval_ptr_dtor(&zclient);
-        zval_ptr_dtor(&zgetprops);
-        zval_ptr_dtor(&zputprops);
-        zval_ptr_dtor(&zdelprops);
-
-        PS_SET_MOD_DATA(NULL);
-
-        return FAILURE;
-    } else {
-        session_data = ecalloc(1, sizeof(riak_session_data));
-
-        session_data->zbucket = zbucket;
-        session_data->zclient = zclient;
-        session_data->zgetprops = zgetprops;
-        session_data->zputprops = zputprops;
-        session_data->zdelprops = zdelprops;
-        session_data->session_name = estrdup(session_name);
-
-        PS_SET_MOD_DATA(session_data);
-
-        return SUCCESS;
+        goto cleanup_zvals;
     }
+
+    php_url_free(purl);
+
+    data->session_name = estrdup(session_name);
+
+    PS_SET_MOD_DATA(data);
+
+    return SUCCESS;
+
+cleanup_zvals:
+    zval_dtor(&data->zgetprops);
+    zval_dtor(&data->zputprops);
+    zval_dtor(&data->zdelprops);
+    zval_dtor(&data->zbucket);
+
+cleanup_client:
+    zval_dtor(&data->zclient);
+
+    efree(data);
+
+cleanup_purl:
+    php_url_free(purl);
+
+    PS_SET_MOD_DATA(NULL);
+
+cleanup:
+    return FAILURE;
 }
 /* }}} */
 
@@ -189,40 +190,36 @@ PS_CLOSE_FUNC(riak) /* {{{ */
 
 PS_READ_FUNC(riak) /* {{{ */
 {
-    zval *zoutput, *zkey, *zdata;
+    zval zoutput, zkey, *zdata;
     riak_session_data *data = PS_GET_MOD_DATA();
     *vallen = 0;
 
-    MAKE_STD_ZVAL(zkey);
-    ZVAL_STRING(zkey, key, 1);
+    ZVAL_STRING(&zkey, key, 1);
 
-    MAKE_STD_ZVAL(zoutput);
-    object_init_ex(zoutput, riak_output_ce);
+    object_init_ex(&zoutput, riak_output_ce);
 
     RIAK_CALL_METHOD2(RiakBucket, get, zoutput, data->zbucket, zkey, data->zgetprops);
 
-    if (!EG(exception) && Z_TYPE_P(zoutput) == IS_OBJECT) {
-        zval *zobjlist;
+    if (EG(exception)) {
+        zend_clear_exception(TSRMLS_C);
+    } else if (Z_TYPE_P(zoutput) == IS_OBJECT) {
+        zval zobjlist;
 
-        MAKE_STD_ZVAL(zobjlist);
-        RIAK_CALL_METHOD(Riak_Output_Output, getObjectList, zobjlist, zoutput);
-        if (Z_TYPE_P(zobjlist) == IS_OBJECT) {
-            zval *zobject;
-            MAKE_STD_ZVAL(zobject);
-            RIAK_CALL_METHOD(Riak_Object_List, first, zobject, zobjlist);
-            if (Z_TYPE_P(zobject) == IS_OBJECT) {
-                zdata = zend_read_property(riak_object_ce, zobject, "content", sizeof("content")-1, 1 TSRMLS_CC);
+        RIAK_CALL_METHOD(Riak_Output_Output, getObjectList, &zobjlist, zoutput);
+        if (Z_TYPE(zobjlist) == IS_OBJECT) {
+            zval zobject;
+            RIAK_CALL_METHOD(Riak_Object_List, first, &zobject, &zobjlist);
+            if (Z_TYPE(zobject) == IS_OBJECT) {
+                zdata = zend_read_property(riak_object_ce, &zobject, "content", sizeof("content")-1, 1 TSRMLS_CC);
                 if (zdata->type == IS_STRING && Z_STRLEN_P(zdata) > 0) {
                     *vallen = Z_STRLEN_P(zdata);
                     *val = emalloc(Z_STRLEN_P(zdata));
                     memcpy(*val, Z_STRVAL_P(zdata), Z_STRLEN_P(zdata));
                 }
             }
-            zval_ptr_dtor(&zobject);
+            zval_dtor(&zobject);
         }
-        zval_ptr_dtor(&zobjlist);
-    } else if (EG(exception)) {
-        zend_clear_exception(TSRMLS_C);
+        zval_dtor(&zobjlist);
     }
 
     if (*vallen == 0) {
